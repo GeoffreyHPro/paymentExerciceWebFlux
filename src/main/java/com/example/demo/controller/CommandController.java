798c6f5d70818c2c0bd7e8bebfeb334f06e9c771
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-01-23 00:06:46 +0100

    docs: add swagger doc in commandController

--- src/main/java/com/example/demo/controller/CommandController.java
+++ src/main/java/com/example/demo/controller/CommandController.java
@@ -18,6 +18,10 @@ import com.example.demo.request.CommandRequest;
 import com.example.demo.service.CommandService;
 import com.example.demo.service.PaymentMapper;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -30,7 +34,12 @@ public class CommandController {
     @Autowired
     private PaymentMapper paymentMapper;
 
+    
     @GetMapping("/{id}")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "The command is successfully get", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CommandDTO.class))),
+        @ApiResponse(responseCode = "404", description = "The command is not found", content = @Content(mediaType = "application/json"))
+    })
     public Mono<ResponseEntity<CommandDTO>> getCommand(@PathVariable int id) throws NotFoundException {
         return commandService.getCommand(id)
                 .map(command -> ResponseEntity.status(200).body(paymentMapper.toCommandDTO(command)))
@@ -38,6 +47,12 @@ public class CommandController {
     }
 
     @PostMapping("/{idPayment}")
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "The command is successfully created", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CommandDTO.class))),
+        @ApiResponse(responseCode = "404", description = "The payment is not found to link with the command" ),
+        @ApiResponse(responseCode = "400", description = "The request body is not correctly give. Price or quantity is wrong"),
+        @ApiResponse(responseCode = "401", description = "You cannot add command to the payment id gived. The payment must be in IN_PROGRESS status")
+    })
     public Mono<ResponseEntity<CommandDTO>> addCommand(@PathVariable int idPayment,
             @RequestBody CommandRequest commandRequest)
             throws NegativeValueException, NulValueException, PaymentStatusException {
@@ -49,8 +64,8 @@ public class CommandController {
         return commandService.addCommand(idPayment, commandRequest)
                 .map(command -> ResponseEntity.status(201).body(paymentMapper.toCommandDTO(command)))
                 .onErrorResume(NotFoundException.class, e -> Mono.just(ResponseEntity.status(404).body(null)))
-                .onErrorResume(NulValueException.class, e -> Mono.just(ResponseEntity.status(404).body(null)))
-                .onErrorResume(PaymentStatusException.class, e -> Mono.just(ResponseEntity.status(404).body(null)))
-                .onErrorResume(NegativeValueException.class, e -> Mono.just(ResponseEntity.status(404).body(null)));
+                .onErrorResume(NulValueException.class, e -> Mono.just(ResponseEntity.status(400).body(null)))
+                .onErrorResume(PaymentStatusException.class, e -> Mono.just(ResponseEntity.status(401).body(null)))
+                .onErrorResume(NegativeValueException.class, e -> Mono.just(ResponseEntity.status(400).body(null)));
     }
 }
