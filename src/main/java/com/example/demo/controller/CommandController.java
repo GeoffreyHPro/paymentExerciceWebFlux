b9a52d976e65d204c024f1a148686042ed1458b3
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-01-22 12:05:07 +0100

    feat: add CommandController

---
+++ src/main/java/com/example/demo/controller/CommandController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Command;
+import com.example.demo.service.CommandService;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping(path = "/command")
+public class CommandController {
+
+    @Autowired
+    private CommandService commandService;
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<Command>> getCommand(@PathVariable int id) throws NotFoundException {
+
+        return commandService.getCommand(id).map(command -> ResponseEntity.status(200).body(command))
+                .onErrorResume(NotFoundException.class, e -> Mono.just(ResponseEntity.status(404).body(null)));
+
+    }
+}
