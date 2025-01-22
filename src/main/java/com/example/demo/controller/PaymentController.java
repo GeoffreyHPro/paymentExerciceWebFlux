537a64540fb2b028e2bc5e871926d4f129307921
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-01-22 16:27:47 +0100

    feat: add read and create payment

--- src/main/java/com/example/demo/controller/PaymentController.java
+++ src/main/java/com/example/demo/controller/PaymentController.java
@@ -5,10 +5,13 @@ import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Payment;
+import com.example.demo.service.CommandService;
 import com.example.demo.service.PaymentService;
 
 import reactor.core.publisher.Mono;
@@ -16,15 +19,37 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping(path = "/payment")
 public class PaymentController {
-    
+
     @Autowired
     private PaymentService paymentService;
 
+    @Autowired
+    private CommandService commandService;
+
     @GetMapping("/{id}")
-    public Mono<ResponseEntity<Payment>> getPayment(@PathVariable int id) throws NotFoundException {
+    public Mono<ResponseEntity<?>> getPayment(@PathVariable int id) throws NotFoundException {
+        return paymentService.getPayment(id)
+                .flatMap(payment -> {
+                    if (payment == null) {
+                        return Mono.just(ResponseEntity.status(404).body(null));
+                    }
+                    return commandService.getCommands(id)
+                            .collectList()
+                            .map(commands -> {
+                                payment.setListeCommands(commands);
+                                return ResponseEntity.status(200).body(payment);
+                            });
+                }).onErrorResume(NotFoundException.class, e -> Mono.just(ResponseEntity.status(404).body(null)));
+    }
 
-        return paymentService.getPayment(id).map(command -> ResponseEntity.status(200).body(command))
-                .onErrorResume(NotFoundException.class, e -> Mono.just(ResponseEntity.status(404).body(null)));
-
+    @PostMapping
+    public Mono<ResponseEntity<?>> addPayment() {
+        try {
+            Mono<Payment> payment = paymentService.addPayment();
+            return Mono.just(ResponseEntity.status(201).body(payment));
+        } catch (Exception e) {
+            System.out.println(e);
+            return Mono.just(ResponseEntity.status(201).body("The payment is created"));
+        }
     }
 }
