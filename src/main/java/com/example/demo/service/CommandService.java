fb4c496c1294c3138dd477b61cf6f3918127671e
Geoffrey HERMAN <geoffreyherman19021998@gmail.com>
2025-01-22 16:28:42 +0100

    feat: add function to get flux and add command in CommandService

--- src/main/java/com/example/demo/service/CommandService.java
+++ src/main/java/com/example/demo/service/CommandService.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.exception.NegativeValueException;
+import com.example.demo.exception.NulValueException;
 import com.example.demo.model.Command;
 import com.example.demo.repository.CommandRepository;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -18,6 +21,17 @@ public class CommandService {
     public Mono<Command> getCommand(int id) throws NotFoundException {
         Mono<Command> command = commandRepository.findById(id);
         return command.switchIfEmpty(Mono.error(new NotFoundException()));
+    }
+
+    public Flux<Command> getCommands(int idPayment) {
+        Flux<Command> command = commandRepository.findByPaymentId(idPayment);
+        return command.switchIfEmpty(Mono.error(new NotFoundException()));
+    }
+
+    public Mono<Command> addCommand() throws NegativeValueException, NulValueException {
+        Command command = new Command("ball", "16546", 2, 20.0);
+        command.setPaymentId(1);
+        return commandRepository.save(command);
     }
 
 }
